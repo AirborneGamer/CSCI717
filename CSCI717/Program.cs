93bec5b44b292b6d38b4e347d0516197322aa1e7
Adam <Adam@AirborneGamer.com>
2017-09-25 20:48:30 -0500

    Add project files.

---
+++ CSCI717/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSCI717
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
